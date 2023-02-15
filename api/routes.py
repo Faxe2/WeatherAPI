37d5d15b76032611b28b182475a6a3abd700e759
Faxe <71709916+Faxe2@users.noreply.github.com>
2023-02-15 19:56:30 +0100

    Added Fix for IndexError, Added Comments, more json objects, added caching

--- api/routes.py
+++ api/routes.py
@@ -2,18 +2,40 @@ from flask import Flask, request, jsonify
 import functions
 from . import app
 
+# Create a dictionary to store the cached responses for each city header
+url_cache = {}
+
+# Define the route for '/v1/weather' with HTTP method GET
 @app.route('/v1/weather', methods=['GET'])
 def weather_route() -> None:
+    # Get the 'city' header from the request
     city_header = request.headers.get('city')
+    # If the 'city' header is missing or empty
     if not city_header:
-       return functions.api_funcs.get_message_dict('Invalid City!')
-   
-    weather_info = functions.api_funcs.get_weather(city_header)
-    if weather_info is None:
+        # Store the error message in the cache dictionary for this city header
+        url_cache[city_header] = functions.api_funcs.get_message_dict('Invalid City!')
+        # Return the error message as a response
         return functions.api_funcs.get_message_dict('Invalid City!')
     
-    temp, description, pressure, humidity, longitude, latitude = weather_info
-    
+    # If the 'city' header is already in the cache dictionary
+    if city_header in url_cache:
+        # Return the cached response for this city header
+        return url_cache[city_header]
+
+    # Get the weather information for the given city header using a function from the 'functions' module
+    weather_info = functions.api_funcs.get_weather(city_header)
+    # If there is no weather information for the given city header
+    if weather_info is None:
+        # Store the error message in the cache dictionary for this city header
+        url_cache[city_header] = functions.api_funcs.get_message_dict('Invalid City!')
+        # Print the cache dictionary for debugging purposes
+        print(url_cache)
+        # Return the error message as a response
+        return functions.api_funcs.get_message_dict('Invalid City!')
+    # Print the cache dictionary for debugging purposes
+    print(url_cache)
+    # Extract the weather information and store it in a dictionary
+    temp, description, pressure, humidity, longitude, latitude, temp_min, temp_max = weather_info
     json_data = dict(
         location=dict(
             name=city_header,
@@ -24,8 +46,14 @@ def weather_route() -> None:
             temperature=temp,
             humidity=humidity,
             pressure=pressure,
+            temp_min=temp_min,
+            temp_max=temp_max,
             description=description
         )
     )
-    
-    return jsonify(json_data)
+    # Cache the weather information in the cache dictionary for this city header
+    url_cache[city_header] = jsonify(json_data)
+    # Print the cache dictionary for debugging purposes
+    print(url_cache)
+    # Return the cached response for this city header
+    return url_cache[city_header]
--- functions/api_funcs.py
+++ functions/api_funcs.py
@@ -11,11 +11,15 @@ def get_message_dict(msg: str) -> dict:
 def get_weather(city: str) -> Tuple[float, str, float, float, float, float]:
     """This function gets the given city and then fetches the data from openweathermap"""
     with requests.Session() as session:
+        # get city id from API
         response = session.get(f'https://openweathermap.org/data/2.5/find?q={city}&appid=439d4b804bc8187953eb36d2a8c26a02&units=metric').json()
-        if 'list' not in response:
+        if 'list' not in response or not response['list']:
             return None
         city_id = response['list'][0]['id']
+        # use city id to get final weather info
         final_response = session.get(f'https://openweathermap.org/data/2.5/weather?id={city_id}&appid=439d4b804bc8187953eb36d2a8c26a02').json()
+        # extract relevant data from response
         temp, desc, pressure, humidity = final_response['main']['temp'], final_response['weather'][0]['description'], final_response['main']['pressure'], final_response['main']['humidity']
-        longitude, latitude = final_response['coord']['lon'], final_response['coord']['lat']
-        return temp, desc, pressure, humidity, longitude, latitude
+        longitude, latitude, temp_min, temp_max = final_response['coord']['lon'], final_response['coord']['lat'], final_response['main']['temp_min'], final_response['main']['temp_max']
+        # return a tuple of relevant data
+        return temp, desc, pressure, humidity, longitude, latitude, temp_min, temp_max
